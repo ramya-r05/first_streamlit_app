b167ce73d1b0f829630ec53a23946104aefd8336
ramya-r05 <148845713+ramya-r05@users.noreply.github.com>
2023-10-25 17:44:23 +0530

    Update streamline_app.py
    added breaktast menu header and 3 Items

--- streamline_app.py
+++ streamline_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.text ('Breakfast Menu')
-
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text ('Hard-Boiled Free-Range Egg')
+streamlit.header( 'Breakfast Favorites')
+streamlit.text(' omega 3 & Blueberry Oatmeal')
+streamlit.text('@ Kale, Spinach & Rocket Smoothie')
+streamlit.text('4 Hard-Boiled Free-Range Egg')
+streamlit. text('0 Avocado Toast")
