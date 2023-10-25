0a3395cba2db1066ea2d3d0f68fb9dabd1eff7b8
ramya-r05 <148845713+ramya-r05@users.noreply.github.com>
2023-10-25 17:56:29 +0530

    Update streamline_app.py
    correcting error

--- streamline_app.py
+++ streamline_app.py
@@ -11,11 +11,11 @@ streamlit. text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
-my fruit list = pandas.read csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit macros.txt")
-my fruit list = my fruit list.set index('Fruit')
+my_fruit_list = pandas.read csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit macros.txt")
+my_fruit_list = my fruit list.set index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list (my fruit list.index))
+streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
 
 #display the table on the page
-streamlit.dataframe (my fruit list)
+streamlit.dataframe (my_fruit_list)
